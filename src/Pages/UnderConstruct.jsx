d0e932fa13e617ed709a4bb8061a31eb0f173bb1
Inder-designer <bhupinder7814@gmail.com>
2024-10-09 12:52:09 +0530

    first commit

--- src/Pages/UnderConstruct.jsx
+++ src/Pages/UnderConstruct.jsx
@@ -5,14 +5,19 @@ import img2 from "../assets/images/underConstruct2.png";
 const UnderConstruct = () => {
   return (
     <div className="py-0 bg-white">
-        {/* <img src={img2} className="h-[600px] mx-auto" /> */}
+      {/* <img src={img2} className="h-[600px] mx-auto" /> */}
       <div className="container mx-auto">
-        <div className="flex items-center gap-10 justify-center max-w-[1080px] mx-auto">
+        <div className="flex items-center gap-10 justify-center max-w-[1280px] mx-auto">
           <div className="">
             {/* <p className="text-6xl text-primary font-bold">Oops!</p> */}
-            <p className="text-2xl word- text-text1 font-bold mt-8 uppercase">
-              <span className="text-primary">Website</span> is Under <br />{" "}
-              <span className="">Construction and Enhancement</span>
+            <p className="text-3xl font-bold mt-8 text-primary uppercase">Welcome to Mexxiss Technology</p>
+            <p className="text-xl text-text1 font-bold mt-8 ">
+              {/* <span className="text-primary">Website</span> is Under <br />{" "} */}
+              <span className=" uppercase">
+                Currently Our Website is Under <br />{" "}
+                <span className="italic">Construction</span> and 
+                <span className="italic"> Enhancement{" "}</span>
+              </span>
             </p>
             <button className="rounded text-xl text-white font-medium mt-8 uppercase bg-red-gradient px-4 py-2">
               Coming Soon
